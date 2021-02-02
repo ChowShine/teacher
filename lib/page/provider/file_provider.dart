bdcd7dec4d8984e5d33e02f0a0a6a0bb1c181220
jsshinechow <603321200@qq.com>
2021-02-02 15:49:30 +0800

    优化FileProvider

--- lib/page/provider/file_provider.dart
+++ lib/page/provider/file_provider.dart
@@ -4,29 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:videochat_package/constants/token/teacher_token.dart';
 import 'package:videochat_package/constants/constants.dart';
 import '../common/common_param.dart';
-import 'package:videochat_package/constants/customMgr/httpMgr.dart';
-import 'dart:io';
-
 
 class FileProvider extends BaseProvider<List<String>> {
-  Future<void> getPhoneCodeReq(int nProjectId,List<File> lsFile) async {
-    if (lsFile != null && lsFile.isNotEmpty) {
-      var formdataMap = await HttpMgr().getFormDataMap(lsFile, {"project_id": nProjectId});
-      FormData formData = FormData.fromMap(formdataMap);
-
-      try {
-        var resp =
-            await Constants.httpTeacherToken.getAuthenticatedClient().post("${gnNetType==0?URL.API_ADDRESS:URLIntranet.API_ADDRESS}upload", data: formData);
-        if (resp != null) {
-          Map mpData = Map<String, dynamic>.from(resp.data);
-          //this.setData(FileModel.fromJson(mpData).data);
-        }
-      } catch (e) {
-        print("$e");
-      }
-    }
-  }
-
   static Future<bool> postSaveReq(
     int student_id,
     int nScore,
